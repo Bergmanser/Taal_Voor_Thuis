b5fb92ced917b6611d9537a2a9505ac25b03d6e8
Nick Bergmans <s1172173@student.windesheim.nl>
2024-02-07 11:05:25 +0100

    css fix confirm password

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-// var x = document.getElementById("login");
-// var y = document.getElementById("register");
-// var z = document.getElementById("btn");
 
-// // Register and login Function.
-// function register() {
-//     x.style.left = "-400px";
-//     y.style.left = "50px";
-//     z.style.left = "110px";
-// }
-// function login() {
-//     x.style.left = "50px";
-//     y.style.left = "450px";
-//     z.style.left = "0px";
-// }
+function validate_password() {
+
+    let password = document.getElementById('password').value;
+    let confirm_password = document.getElementById('confirm_password').value;
+    if (password != confirm_password) {
+        document.getElementById('wrong_password_alert').style.color = 'red';
+        document.getElementById('wrong_password_alert').innerHTML
+            = '☒ Use same password';
+        document.getElementById('create').disabled = true;
+        document.getElementById('create').style.opacity = (0.4);
+    } else {
+        document.getElementById('wrong_pass_alert').style.color = 'green';
+        document.getElementById('wrong_pass_alert').innerHTML =
+            '🗹 Password Matched';
+        document.getElementById('create').disabled = false;
+        document.getElementById('create').style.opacity = (1);
+    }
+}
+
+function wrong_password_alert() {
+    if (document.getElementById('password').value != "" &&
+        document.getElementById('confirm_password').value != "") {
+        alert("Your response is submitted");
+    } else {
+        alert("Please fill all the fields");
+    }
+}
